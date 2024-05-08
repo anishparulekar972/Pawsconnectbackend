26fed42b4e2202f92f44c7f6991a99d1f36187a4
Ibrahim Cheena <143558610+IbrahimCheena@users.noreply.github.com>
2024-05-07 23:07:49 -0700

    Update api.js
    Updated the router paths for backend to work

--- server/static/scripts/api.js
+++ server/static/scripts/api.js
@@ -16,6 +16,21 @@ router.delete('/removePetProfile', apiController.removePetProfile);
 
 router.post('/createPost', apiController.createPost);
 
+router.post('/user', apiController.getUserProfile);
+
+router.post('/pet_profiles', apiController.getPetProfile);
+
+router.post('/getPosts', apiController.getPosts);
+
+router.post('/updateUserProfile', apiController.updateUserProfile);
+
+router.post('/updatePetProfile', apiController.updatePetProfile);
+
+router.post('/getPetProfile', apiController.getPetProfile);
+
+router.post('/translateText', apiController.translateText);
+
+
 
 
 module.exports = router;
